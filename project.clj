95ca02024f911c6dc4bdceea6a5e435900a42c91
Stephan Eckardt <contact@stephaneckardt.com>
2014-04-24 14:15:44 +0200

    24

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject clojure-euler "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/math.numeric-tower "0.0.4"]])
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/core.logic "0.8.7"]
+                 [org.clojure/math.combinatorics "0.0.7"]])
